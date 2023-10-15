16de9dcc4adff8c5dab2530fa4ea1ccf7693fc0b
Anmol <anmolsirola57@gmail.com>
2023-10-15 15:35:19 +0530

    Adding IPFS option

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -3,3 +3,28 @@ import { useState } from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from '../Marketplace.json';
 import { useLocation } from "react-router";
+
+export default function SellNFT () {
+    const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
+    const [fileURL, setFileURL] = useState(null);
+    const ethers = require("ethers");
+    const [message, updateMessage] = useState('');
+    const location = useLocation();
+
+    //This function uploads the NFT image to IPFS
+    async function OnChangeFile(e) {
+        var file = e.target.files[0];
+        //check for file extension
+        try {
+            //upload the file to IPFS
+            const response = await uploadFileToIPFS(file);
+            if(response.success === true) {
+                console.log("Uploaded image to Pinata: ", response.pinataURL)
+                setFileURL(response.pinataURL);
+            }
+        }
+        catch(e) {
+            console.log("Error during file upload", e);
+        }
+    }
+}    
